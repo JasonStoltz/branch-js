b045508d05902076679e381d7aa53ba2008355b5
Jason Stoltzfus <jastoltz24@gmail.com>
2015-10-16 07:35:22 -0400

    Simplify clone function

--- src/clone.js
+++ src/clone.js
@@ -1,29 +1,24 @@
 //http://stackoverflow.com/questions/728360/most-elegant-way-to-clone-a-javascript-object#answer-728694
 export default function clone(obj) {
-  var copy;
 
   // Handle the 3 simple types, and null or undefined
   if (null == obj || "object" != typeof obj) return obj;
 
   // Handle Date
   if (obj instanceof Date) {
-    copy = new Date();
+    let copy = new Date();
     copy.setTime(obj.getTime());
     return copy;
   }
 
   // Handle Array
   if (obj instanceof Array) {
-    copy = [];
-    for (var i = 0, len = obj.length; i < len; i++) {
-      copy[i] = clone(obj[i]);
-    }
-    return copy;
+    return obj.map(e => clone(e));
   }
 
   // Handle Object
   if (obj instanceof Object) {
-    copy = {};
+    let copy = {};
     for (var attr in obj) {
       if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
     }
